31fe352119334ad91b4f68b0e540a0359f4b6f8b
Jacob Moyer <jamoyer@iastate.edu>
2015-11-19 05:31:58 -0600

    Adding index.php link to Dice Roller. Also cleaning up indentation in index.php - mix of tabs and 2 spaces are now all 4 spaces

--- index.php
+++ index.php
@@ -1,43 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Pathfinder Toolkit</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="./css/bootstrap.min.css" media="screen">
-    <link rel="stylesheet" href="./css/bootswatch.min.css">
-  </head>
-  <body>
-  <?php 
-  include_once('./stats.php');
-  include_once('./header/header.php'); 
-  ?>
-    <div class="container">
-
-      <div class="page-header" id="banner">
-        <div class="row">
-          <div class="col-lg-8 col-md-7 col-sm-6">
-            <h1>Welcome to D20ToolKit!</h1>
-            <p class="lead">Here you will find tools for The Pathfinder Roleplaying Game, as well as other useful resources.</p>
-			<div class = "intro">
-			<h3>Custom Tools</h3>
-			<h4><a href="./tools/sacredgeo/sacredgeo.php">Sacred Geometry Calculator</a></h4>
-			<h4><a href="./tools/arithmancy/arithmancy.php">Arithmancy Calculator</a></h4>
-			</div>
-			<h3>Linked Resources</h3>
-			<h4><a href="http://www.d20pfsrd.com/">D20PFSRD</a></h4>
-			<h4><a href="http://combatmanager.com/">Combat Manager</a></h4>
-			<h4><a href="http://jesterraiin.dropmark.com/165340">More Resources</a></h4>
-          </div>
-          <div class="col-lg-4 col-md-5 col-sm-6">
-            <div class="sponsor">
-
+    <head>
+        <meta charset="utf-8">
+        <title>Pathfinder Toolkit</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./css/bootstrap.min.css" media="screen">
+        <link rel="stylesheet" href="./css/bootswatch.min.css">
+    </head>
+    <body>
+        <?php
+            include_once('./stats.php');
+            include_once('./header/header.php');
+        ?>
+        <div class="container">
+            <div class="page-header" id="banner">
+                <div class="row">
+                    <div class="col-lg-8 col-md-7 col-sm-6">
+                        <h1>Welcome to D20ToolKit!</h1>
+                        <p class="lead">Here you will find tools for The Pathfinder Roleplaying Game, as well as other useful resources.</p>
+                        <div class = "intro">
+                            <h3>Custom Tools</h3>
+                            <h4><a href="./tools/sacredgeo/sacredgeo.php">Sacred Geometry Calculator</a></h4>
+                            <h4><a href="./tools/arithmancy/arithmancy.php">Arithmancy Calculator</a></h4>
+                            <h4><a href="./tools/diceroller/diceroller.php">Dice Roller</a></h4>
+                        </div>
+                        <h3>Linked Resources</h3>
+                        <h4><a href="http://www.d20pfsrd.com/">D20PFSRD</a></h4>
+                        <h4><a href="http://combatmanager.com/">Combat Manager</a></h4>
+                        <h4><a href="http://jesterraiin.dropmark.com/165340">More Resources</a></h4>
+                    </div>
+                    <div class="col-lg-4 col-md-5 col-sm-6">
+                        <div class="sponsor">
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-
-    </div>
-	
-  </body>
+    </body>
 </html>
