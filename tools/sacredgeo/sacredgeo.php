94076d8c6483588ea4af737fe61b37d7451d91f3
Jacob Moyer <jamoyer@iastate.edu>
2015-11-21 00:28:16 -0600

    Made input format more clear to user. (hopefully)

--- tools/sacredgeo/sacredgeo.php
+++ tools/sacredgeo/sacredgeo.php
@@ -15,25 +15,24 @@
         ?>
         <div class="container">
             <h1>Pathfinder Sacred Geometry Solver</h1>
-
             <h6>Algorithm by Matt Clucas - Web version by Jacob Moyer</h6>
 
             <a href="http://www.d20pfsrd.com/feats/general-feats/sacred-geometry">See the rules.</a>
-
             <a href="https://play.google.com/store/apps/details?id=com.clucasprojects.sacredgeometry&hl=en">Get the Android app.</a>
 
             </br>
             </br>Spell Level
             <select id="spellLevelSelect"></select>
-            </br>
+            </br></br>
+            <label>Enter one digit for each dice</label></br>
+            <label>Example - rolled: 5, 6, 3, 2, 4, 4 -> input: "563244"</label>
             </br>Rolled Numbers
             <input type="text" id="rolledNumbers" />
             </br>
             </br>
             <button id="calculateButton">Wizard!</button>
 
-            <h4>Result</h4>
-
+            <h4>Results</h4>
             <div id="result"></div>
         </div>
     </body>
