91f1fbf8a5481379dac23cd18b11f700e6dcc3f0
Jacob Moyer <jamoyer@iastate.edu>
2015-11-21 00:17:12 -0600

    Adding window.onload function to make sure everything loads correctly

--- tools/sacredgeo/sacredgeo.js
+++ tools/sacredgeo/sacredgeo.js
@@ -1,7 +1,9 @@
-function isValidNumber(evt) {
+function isValidNumber(evt)
+{
     evt = (evt) ? evt : window.event;
     var charCode = (evt.which) ? evt.which : evt.keyCode;
-    if (charCode > 31 && (charCode < 49 || charCode > 56)) {
+    if (charCode > 31 && (charCode < 49 || charCode > 56))
+    {
         return false;
     }
     return true;
@@ -21,82 +23,100 @@ var PRIME_CONSTANTS = [
     [101, 103, 107]
 ];
 
-var MAX_SPELL_LEVEL = 9;
-var MIN_SPELL_LEVEL = 1;
+window.onload = function()
+{
+    var MAX_SPELL_LEVEL = 9;
+    var MIN_SPELL_LEVEL = 1;
 
-// populate select form
-var spellLevelSelect = document.getElementById('spellLevelSelect');
-for (var i = MIN_SPELL_LEVEL; i <= MAX_SPELL_LEVEL; i++) {
-    var opt = document.createElement('option');
-    opt.value = i;
-    opt.innerHTML = i;
-    spellLevelSelect.appendChild(opt);
-}
-
-// attach keypress handler to rolledNumbers text box
-var rolledNumbers = document.getElementById('rolledNumbers');
-rolledNumbers.onkeypress = function (event) {
-    return isValidNumber(event);
-};
-
-document.getElementById('calculateButton').addEventListener("click", function () {
-    var invalidInputMsg = "Please enter at least 2 numbers from 1 through 8.";
-    var resultsDiv = document.getElementById('result');
-    var spellLevel = spellLevelSelect.options[spellLevelSelect.selectedIndex].value;
-
-    // validate rolls
-    var rollsText = rolledNumbers.value;
-    if (rollsText.length < 2) {
-        resultsDiv.innerHTML = invalidInputMsg;
-        return;
+    // populate select form
+    var spellLevelSelect = document.getElementById('spellLevelSelect');
+    for (var i = MIN_SPELL_LEVEL; i <= MAX_SPELL_LEVEL; i++)
+    {
+        var opt = document.createElement('option');
+        opt.value = i;
+        opt.innerHTML = i;
+        spellLevelSelect.appendChild(opt);
     }
-    var rolls = [];
-    for (var i = 0; i < rollsText.length; i++) {
-        var roll = parseInt(rollsText.charAt(i));
-        // check for invalid number
-        if (!isValidNumber(roll)) {
+
+    // attach keypress handler to rolledNumbers text box
+    var rolledNumbers = document.getElementById('rolledNumbers');
+    rolledNumbers.onkeypress = function (event) 
+    {
+        return isValidNumber(event);
+    };
+
+    document.getElementById('calculateButton').addEventListener("click", function ()
+    {
+        var invalidInputMsg = "Please enter at least 2 numbers from 1 through 8.";
+        var resultsDiv = document.getElementById('result');
+        var spellLevel = spellLevelSelect.options[spellLevelSelect.selectedIndex].value;
+
+        // validate rolls
+        var rollsText = rolledNumbers.value;
+        if (rollsText.length < 2)
+        {
             resultsDiv.innerHTML = invalidInputMsg;
             return;
         }
-        rolls.push(roll);
-    }
-
-    // do the calculation and display the results
-    var result = calculate(spellLevel, rolls);
-    if (result) {
-        var diceIndex = 0;
-        var operationsIndex = 0;
-        var display = "" + result.dice[0] + result.operations[0] + result.dice[1];
-        for (var i = 1; i < result.operations.length; i++) {
-            // wrap with parenthesis to show correct ordering
-            display = "(" + display + ") " + result.operations[i] + result.dice[i + 1];
+        var rolls = [];
+        for (var i = 0; i < rollsText.length; i++)
+        {
+            var roll = parseInt(rollsText.charAt(i));
+            // check for invalid number
+            if (!isValidNumber(roll)) {
+                resultsDiv.innerHTML = invalidInputMsg;
+                return;
+            }
+            rolls.push(roll);
         }
-        display += " = " + result.result + " in " + result.loops + " guesses.";
-        resultsDiv.innerHTML = display;
-    } else {
-        var primes = PRIME_CONSTANTS[spellLevel - 1];
-        resultsDiv.innerHTML = "Sorry, no result found. You need " + primes[0] + ", " + primes[1] + ", or " + primes[2] + ".";
-    }
-});
 
-function calculate(spellLevel, rolls) {
-    function isPrime(total) {
+        // do the calculation and display the results
+        var result = calculate(spellLevel, rolls);
+        if (result)
+        {
+            var diceIndex = 0;
+            var operationsIndex = 0;
+            var display = "" + result.dice[0] + result.operations[0] + result.dice[1];
+            for (var i = 1; i < result.operations.length; i++)
+            {
+                // wrap with parenthesis to show correct ordering
+                display = "(" + display + ") " + result.operations[i] + result.dice[i + 1];
+            }
+            display += " = " + result.result + " in " + result.loops + " guesses.";
+            resultsDiv.innerHTML = display;
+        }
+        else
+        {
+            var primes = PRIME_CONSTANTS[spellLevel - 1];
+            resultsDiv.innerHTML = "Sorry, no result found. You need " + primes[0] + ", " + primes[1] + ", or " + primes[2] + ".";
+        }
+    });
+}
+
+function calculate(spellLevel, rolls)
+{
+    function isPrime(total)
+    {
         var primes = PRIME_CONSTANTS[spellLevel - 1];
-        for (var i = 0; i < primes.length; i++) {
-            if (total == primes[i]) {
+        for (var i = 0; i < primes.length; i++)
+        {
+            if (total == primes[i])
+            {
                 return true;
             }
         }
         return false;
     }
 
-    function shuffle(array) {
+    function shuffle(array)
+    {
         var counter = array.length;
         var temp;
         var index;
 
         // While there are elements in the array
-        while (counter > 0) {
+        while (counter > 0)
+        {
             // Pick a random index
             index = Math.floor(Math.random() * counter);
 
@@ -114,22 +134,27 @@ function calculate(spellLevel, rolls) {
 
     var total = 0;
     var operators;
-    for (var iteration = 0; iteration < MAX_ITERATIONS && !isPrime(total); iteration++) {
+    for (var iteration = 0; iteration < MAX_ITERATIONS && !isPrime(total); iteration++)
+    {
         var operators = [];
         rolls = shuffle(rolls);
         total = rolls[0];
         for (var i = 1; i < rolls.length; i++) {
             var operator = OPERATORS[Math.floor(Math.random() * OPERATORS.length)];
             var currentDie = rolls[i];
-            switch (operator) {
+            switch (operator)
+            {
                 case '+':
                     total += currentDie;
                     break;
                 case '-':
                     // avoid negatives or 0s by not doing subtraction when it is not positive
-                    if (total > currentDie) {
+                    if (total > currentDie)
+                    {
                         total -= currentDie;
-                    } else {
+                    }
+                    else
+                    {
                         total *= currentDie;
                         operator = '*';
                     }
@@ -139,9 +164,12 @@ function calculate(spellLevel, rolls) {
                     break;
                 case '/':
                     // avoid negatives or 0s by not doing division when it is not positive
-                    if (total % currentDie == 0) {
+                    if (total % currentDie == 0)
+                    {
                         total /= currentDie;
-                    } else {
+                    }
+                    else
+                    {
                         total += currentDie;
                         operator = '+';
                     }
@@ -152,7 +180,8 @@ function calculate(spellLevel, rolls) {
     }
 
     // either return an object with the solution or null
-    if (isPrime(total)) {
+    if (isPrime(total))
+    {
         return {
             result: total,
             dice: rolls,
