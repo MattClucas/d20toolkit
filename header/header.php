61c4eea45b00ed48415d38aa93f1e60a5494d2c8
Jacob Moyer <jamoyer@iastate.edu>
2016-01-27 16:19:27 -0800

    Adding a link to github in header.php comments.

--- header/header.php
+++ header/header.php
@@ -1,17 +1,24 @@
+<!--
+    Curious about the source code? We try to make this site
+    as simple and minimal as possible. We minify all our
+    css and js files to make the site load faster (really
+    it's just for fun). But all the source code is here:
+    https://github.com/MattClucas/d20toolkit
+-->
 <div class="navbar navbar-default navbar-fixed-top">
-  <div class="container">
-	<div class="navbar-header">
-	  <a href="/index.php" class="navbar-brand">D20ToolKit</a>
-	</div>
-	<div class="navbar-collapse collapse" id="navbar-main">
-	  <ul class="nav navbar-nav">
-		<li>
-		  <a href="/header/about.php">About</a>
-		</li>
-		<li>
-		  <a href="/header/contact.php">Contact</a>
-		</li>
-	  </ul>
-	</div>
-  </div>
+    <div class="container">
+        <div class="navbar-header">
+            <a href="/" class="navbar-brand">D20ToolKit</a>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="/header/about.php">About</a>
+                </li>
+                <li>
+                    <a href="/header/contact.php">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </div>
