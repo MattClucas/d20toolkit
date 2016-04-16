d4832c608bd596462d173498368c1add4f64ec67
Jacob Moyer <jamoyer@iastate.edu>
2016-04-16 16:32:39 -0700

    Adding 404 redirect page.

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php $ROOT = $_SERVER['DOCUMENT_ROOT']; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once($ROOT . '/header/common-head-tags.php'); ?>
+    <title>Pathfinder Toolkit</title>
+</head>
+<body>
+    <?php
+        include_once($ROOT . '/stats.php');
+        include_once($ROOT . '/header/header.php');
+    ?>
+    <div class="container">
+        <h1>We're sorry.</h1>
+        <p>This page does not exist or may have been moved. Try going to the <a href="/">home page</a> to find what you are looking for.</p>
+    </div>
+</body>
+</html>
